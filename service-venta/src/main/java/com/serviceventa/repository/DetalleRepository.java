33dee307df19ca14fab67589cec7c4d316a13440
JOSELIN-CONDORI <yoselin.tintaya.25@gmail.com>
2023-09-25 02:00:44 -0500

    microservicio venta

---
+++ service-venta/src/main/java/com/serviceventa/repository/DetalleRepository.java
@@ -0,0 +1,9 @@
+package com.serviceventa.repository;
+
+import com.quispcs.serviceventa.entity.Detalle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleRepository extends JpaRepository<Detalle, Long> {
+
+
+}
